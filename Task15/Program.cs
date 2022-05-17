11fba2f63bd41868f8378bd402487442a1c23194
Alexander Novikov <a_noivkov@inbox.ru>
2022-05-17 10:11:27 +0300

    Task15 complete

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,7 +1,7 @@
 ﻿
-
+Console.WriteLine("введите номер дня недели: ");
 var day = Convert.ToInt32(Console.ReadLine());
-string[] isWeekend = { "нет", "нет", "нет", "нет", "нет", "Да", "Да", };
+string[] isWeekend = { "нет", "нет", "нет", "нет", "нет", "Да", "Да" };
 if (day > 0 && day < 8)
 {
     Console.WriteLine(isWeekend[day - 1]);
