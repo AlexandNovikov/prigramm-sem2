4c8770248958da97657d903b9b98c8efd8a8ebc6
Alexander Novikov <a_noivkov@inbox.ru>
2022-05-17 16:14:56 +0300

    Task22 Complete

---
+++ Task21/Program.cs
@@ -0,0 +1,19 @@
+﻿// написать программу, которая принимает на вход
+// координаты двух точек и находит расстояние между ними
+// в 2D пространстве
+// пример A (3, 6); B (2,1) ->5,09
+//        A (7, -5); B( 1, -1) ->7,21
+
+Console.WriteLine("Введите координаты первой точки ");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки ");
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("расстояние от первой до второй точки = ");
+
+double distance = Math.Sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+double n = Math.Round(distance, 2); // преобразовывает и округляет до 2 занчения после запятой
+Console.WriteLine(n);
